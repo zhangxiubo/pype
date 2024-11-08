ef33bac74cdd22dc8c8ffcd916742242901c1d8c
Xiubo Zhang <zhangxiubo@gmail.com>
2024-11-08 15:31:39 +0000

    Fixed typo

--- src/pype/__init__.py
+++ src/pype/__init__.py
@@ -79,7 +79,7 @@ def tail(count: int):
 def sort_(key_func):
     return lambda seq: sorted(seq, key=lambda e: key_func(e))
 
-def sort_(key_func):
+def sortx(key_func):
     return lambda seq: sorted(seq, key=lambda e: key_func(*e))
 
 
